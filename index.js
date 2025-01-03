0dd0db745aa995acf4cef1b2281f9c33d6d7f897
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-03 15:37:27 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.post("/user/signup", function(req, res) {
+
+})
+
+app.post("/user/signin", function(req, res) {
+    
+})
+
+app.get("/user/purchases", function(req, res) {
+    
+})
+
+app.get("/courses", function(req, res) {
+    
+})
+
+app.get("/course/purchase", function(req, res) {
+    
+})
+
+app.listen(3000);
