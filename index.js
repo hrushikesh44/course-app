030cf13e74cd81967c438a83f17e31ca8b0cd4f1
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-04 02:58:03 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var dotenv = require("dotenv");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 
 const app = express();
 app.use(express.json());
+dotenv.config();
+var url = process.env.MONGOLAB_URI;
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
@@ -13,7 +16,7 @@ app.use("/admin", adminRouter);
 
 
 async function main() {
-    await mongoose.connect("MONGODB_URL");
+    await mongoose.connect(url);
     app.listen(3000);
 }
 
