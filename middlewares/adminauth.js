20423e650c5e5bf41add8fb52f9667864f04b6ba
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-07 22:30:12 +0530

    id

--- middlewares/adminauth.js
+++ middlewares/adminauth.js
@@ -7,7 +7,7 @@ function auth(req, res, next) {
     const response = jwt.verify(token, JWT_ADMIN_PASSWORD);
 
     if(response){
-        req.adminId = response.id;
+        req.userId = response.id;
         next();
     }else{
         res.status(403).json({
